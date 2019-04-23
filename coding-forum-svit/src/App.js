cc990f4524e605b9c49221bf905d7e246f2b9549
Shashank J <shashankreddy231@gmail.com>
2019-04-23 21:47:00 +0530

    Update App.js
    Simple Starter Update.

--- coding-forum-svit/src/App.js
+++ coding-forum-svit/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi I am <code>Shashank J</code> from Sapphire Coders.
         </p>
         <a
           className="App-link"
