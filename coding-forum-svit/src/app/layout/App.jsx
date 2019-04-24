04579adf6a96870a6f2a9dfee93d1d6425ec969a
Shashank J <shashankreddy231@gmail.com>
2019-04-24 21:50:22 +0530

    adding component

--- coding-forum-svit/src/app/layout/App.jsx
+++ coding-forum-svit/src/app/layout/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className='app'>
-      <h1>Coding Forum</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className='app'>
+        <h1>Coding Forum</h1>
+          <button className='ui icon button'>
+            <i className='smile icon'>
+              CODE
+            </i>
+          </button>
+        <Button icon='smile' content='CODE SEMANTIC'/>
+      </div>
+    );
+  }
 }
 
 export default App;
