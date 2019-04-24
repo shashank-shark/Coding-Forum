9c28bbbfba70eff9f5110a9107725bc0db0bddb8
Shashank J <shashankreddy231@gmail.com>
2019-04-24 22:10:38 +0530

    Added navbar to main page.

--- coding-forum-svit/src/app/layout/App.jsx
+++ coding-forum-svit/src/app/layout/App.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+
+import EventDashBoard from '../../features/event/EventDashBoard/EventDashBoard'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render () {
     return (
       <div className='app'>
-        <h1>Coding Forum</h1>
-          <button className='ui icon button'>
-            <i className='smile icon'>
-              CODE
-            </i>
-          </button>
-        <Button icon='smile' content='CODE SEMANTIC'/>
+        <NavBar />
+        <EventDashBoard />
       </div>
     );
   }
