09e9e041ee9a5bdb55873865f97d9f5a3a44cf08
Shashank J <shashankreddy231@gmail.com>
2019-04-25 22:03:27 +0530

    Added list.

--- coding-forum-svit/src/features/event/EventList/EventListAteendee.jsx
+++ coding-forum-svit/src/features/event/EventList/EventListAteendee.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { List, Image } from 'semantic-ui-react'
 
 class EventListAttendee extends Component {
   render() {
     return (
-      <div>
-        <h1>Event List Attendee</h1>
-      </div>
+      <List.Item>
+          <Image as='user-image' size='mini' circular src='https://randomuser.me/api/portraits/women/40.jpg' />
+      </List.Item>
     )
   }
 }
